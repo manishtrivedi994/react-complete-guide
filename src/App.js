e721a75fb8e51cc6306ab0256170691a20530694
Manish Trivedi <manishtrivedi994@gmail.com>
2020-05-02 11:19:21 +0530

    Error Boudary

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState, Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
 import styled from 'styled-components'
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary'
 
 
 const StyledButton = styled.button`
@@ -81,12 +82,13 @@ class App extends Component {
         persons = (
           <div>
             {this.state.persons.map( (person, index) => {
-              return <Person
-                click= {() => this.deletePersonHandler(index)}
-                name = {person.name}
-                age = {person.age}
-                key = {person.id}
-                changed = {(event) => this.nameChangedHandler(event, person.id)}/>
+              return <ErrorBoundary key = {person.id}>
+                <Person
+                  click= {() => this.deletePersonHandler(index)}
+                  name = {person.name}
+                  age = {person.age}
+                  changed = {(event) => this.nameChangedHandler(event, person.id)}/>
+              </ErrorBoundary>
             })}
           </div>
         );
